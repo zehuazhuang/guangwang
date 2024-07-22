799fa8ee3990b4ef3cf03cdf5ba849932604c810
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2024-07-22 16:46:16 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        wavestella662@gmail.com
+        HaithemSalgado9@gmail.com
       </div>
     </div>
   </div>
