41b6fcf018265e217f9ac4639a745a8566b260dd
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2024-08-28 15:28:53 +0800

    yx

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        raoshuo0701@icloud.com
+        wutian536172@icloud.com
       </div>
     </div>
   </div>
