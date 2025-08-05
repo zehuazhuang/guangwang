bb6acdb7b585ab3ccf03938501058faba05a3936
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2025-08-05 10:45:19 +0800

    修改邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        Lsb576358@icloud.com
+        qbqgky55285@icloud.com 
       </div>
     </div>
 
