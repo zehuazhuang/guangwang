0e83a4f99fb2b59a5a7493f370f5945f7fd186e6
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2025-09-16 13:34:29 +0800

    修改yx

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        zzj583799@icloud.com
+        yangyanting0525@icloud.com
       </div>
     </div>
 
