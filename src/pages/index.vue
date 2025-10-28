22b9101fcbb2e7e1ecd8949194dc00ae64d5b004
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2025-10-28 18:09:19 +0800

    邮箱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        zhoulinlei0406@icloud.com
+        loujunhao2004@icloud.com
       </div>
     </div>
 
